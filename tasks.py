5fb626ea295d8e750aa7f05299276013becd70df
Mehdi EL KOUHEN <melkouhen@ippon.fr>
2021-09-14 17:46:37 +0200

    INIT

--- tasks.py
+++ tasks.py
@@ -1,52 +1,82 @@
 #!/usr/bin/python
 import os
 import platform
-import shutil
-import subprocess
-import sys
-import wget
+
 from invoke import task
-from configobj import ConfigObj
-from psutil.tests import process_namespace
 
 
 def _project_name():
     return "helloworld"
 
+
 def _default_stage():
     return "develop"
+
 
 def _aws_region():
     return "eu-west-3"
 
+
 def _cicd_bucket():
     return "cicd-bucket-eu-west-3"
+
 
 def _venv_dir():
     if platform.system() == "Windows":
         return "venv"
     return ".venv"
 
+
 def _activate():
     if platform.system() == "Windows":
         return "call %s/Scripts/activate.bat" % _venv_dir()
-    return "source %s/bin/activate" % _venv_dir()    
+    return "source %s/bin/activate" % _venv_dir()
+
 
 def _pty():
     if platform.system() == "Windows":
         return False
     return True
 
+
+@task
+def stack_build(ctx):
+    ctx.run(f"sam build --region {_aws_region()} --use-container")
+
+
+@task(pre=[stack_build])
+def stack_deploy(ctx, stage=_default_stage()):
+    with ctx.prefix(_activate()):
+        ctx.run(f"aws s3 cp spec/api-spec.yaml s3://{_cicd_bucket()}/spec/api-spec.yaml")
+        ctx.run(f"sam package --s3-bucket {_cicd_bucket()} --output-template-file packaged.yaml")
+        ctx.run(f"sam deploy packaged.yaml "
+                f"--capabilities CAPABILITY_IAM "
+                f"--parameter-overrides "
+                f"StageName={stage} "
+                f"--s3-bucket {_cicd_bucket()} "
+                f"--stack-name {_project_name()}-{stage} "
+                f"--region {_aws_region()} ")
+
+
+@task
+def stack_delete(ctx, stage=_default_stage()):
+    ctx.run(f"aws cloudformation delete-stack --stack-name {_project_name()}-{stage} --region {_aws_region()}")
+
+
+@task
+def test(ctx, stage=_default_stage()):
+    with ctx.prefix(_activate()):
+        os.environ["TABLE_NAME"] = f"{_project_name()}-{stage}"
+        ctx.run(f"TABLE_NAME={_project_name()}-{stage}; pytest tests")
+
+
 @task
 def venv(ctx):
     """Create virtualenv"""
     ctx.run("python -m venv %s" % _venv_dir(), pty=_pty())
     with ctx.prefix(_activate()):
-        ctx.run("python -m pip install --upgrade pip", pty=_pty())
-        ctx.run("python -m pip install --upgrade setuptools", pty=_pty())
-        ctx.run("python -m pip install --upgrade wheel", pty=_pty())
-        ctx.run("python -m pip install --upgrade invoke", pty=_pty())
-        ctx.run("python -m pip install --upgrade wget", pty=_pty())
+        ctx.run("python -m pip install --upgrade pip setuptools wheel invoke wget", pty=_pty())
+
 
 @task(pre=[venv])
 def bootstrap(ctx):
