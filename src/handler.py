57c2dd1b61d1f7c66f5fe71f75ebec68f29003c6
Mehdi EL KOUHEN <melkouhen@ippon.fr>
2021-09-30 17:28:39 +0200

    INIT

--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,6 @@
 from src.domain.song import Song
 from src.domain.song_dao import SongDao
+from src.services.csv_importer import CSVImporter
 from src.core.resources_mgr import ResourcesMgr
 
 import logging
@@ -67,3 +68,15 @@ def find_by_author_and_title(event, context):
         "headers": {"Content-Type": "application/json"},
         "body": json.dumps(entities, default=lambda entity: entity.to_json()),
     }
+
+def import_csv(event, context):
+    dao = SongDao(
+        dynamodb_resource=resources_mgr.dynamodb_resource,
+        dynamodb_client=resources_mgr.dynamodb_client,
+        table_name=resources_mgr.table_name(),
+    )
+
+    CSVImporter(song_dao=dao).import_file(
+        event["Records"][0]["s3"]["bucket"]["name"],
+        event["Records"][0]["s3"]["object"]["key"],
+    )
