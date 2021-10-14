fbb92246fe2962b0d1f180a4959699d8c43ec1b8
Mehdi EL KOUHEN <melkouhen@ippon.fr>
2021-10-14 11:45:21 +0200

    INIT

--- src/services/authorizer.py
+++ src/services/authorizer.py
@@ -3,25 +3,29 @@ class Authorizer:
     def authenticate(self, account, token):
 
         if token == "mysecretpassword":
-            authorization = self.make_authorization("Allow")
+            authorization = self.make_authorization("Allow", account)
             print(authorization)
             return authorization
         else:
-            authorization = self.make_authorization("Deny")
+            authorization = self.make_authorization("Deny", account)
             print(authorization)
             return authorization
 
-    def make_authorization(self, effect):
-        return {
-            "principalId": "user",
-            "policyDocument": {
-                "Version": "2012-10-17",
-                "Statement": [
-                    {
-                        "Effect": effect,
-                        "Action": "execute-api:Invoke",
-                        "Resource": "arn:aws:lambda:eu-west-3:*"
-                    }
-                ]
-            }
+    def make_authorization(self, effect, account):
+
+        response = {}
+
+        response["principalId"] = "user"
+        response["policyDocument"] = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "InvokeStatement",
+                    "Effect": effect,
+                    "Action": "execute-api:Invoke",
+                    "Resource": f"arn:aws:execute-api:eu-west-3:{account}:*"
+                }
+            ]
         }
+
+        return response
